0a5e86d6a5d8094ab457647f4be83a5c361ceccb
Alexander Schofield <alex.schofield816@gmail.com>
2026-09-22 16:54:55 -0400

    selection sort without ai

---
+++ src/algorithms/sorting/comparison/selection-sort/selection-sort.ts
@@ -0,0 +1,16 @@
+const selectionSort = (items: Array<any>, compare: (left: any, right: any) => number): boolean => {
+    if(items.length === 0 || items.length === 1) {
+        return true
+    }
+
+    for (let i = 0; i < items.length; i++) {
+        let smallest_index = i;
+        for (let n = 0; n < items.length; n++) {
+            if(compare(items[smallest_index], items[n]) < 0) smallest_index = n;
+        }
+
+        items[i], items[smallest_index] = items[smallest_index], items[i];
+    }
+
+    return true;
+}
